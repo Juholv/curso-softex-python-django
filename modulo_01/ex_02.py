0d2d0e8e4972e08f13f93a662d241636fd17f41a
Juholv <227845472+Juholv@users.noreply.github.com>
2025-09-01 14:23:03 -0300

    elementos listas em comum

---
+++ modulo_01/ex_02.py
@@ -0,0 +1,8 @@
+lista1= ['carro', 'moto', 'navio', 'avião']
+lista2 = ['avião', 'ônibus', 'caminhão', 'navio']
+
+for elementos1 in lista1:
+    for elementos2 in lista2:
+        if elementos1 == elementos2:
+            print(elementos1)
+        
